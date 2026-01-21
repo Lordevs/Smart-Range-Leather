2343f97a4aa4b9ebbd4a638e28ec26150e51cb8c
WahajHassan883 <wahajhassan007@gmail.com>
2026-01-21 12:17:18 +0500

    feat: Implement and integrate the Manufacturing Process section onto the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HomeHeroSection from "@/components/home-hero-section";
 import { StatsSection } from "@/components/common/stats-section";
 import { AboutSection } from "@/components/about-section";
 import { WhyChooseUs } from "@/components/common/why-choose-us";
+import { ManufacturingProcess } from "@/components/common/manufacturing-process";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <StatsSection />
       <AboutSection />
       <WhyChooseUs />
+      <ManufacturingProcess />
     </main>
   );
 }
