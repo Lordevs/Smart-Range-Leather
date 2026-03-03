d387d557ef74d7bfd85eca5cb59c2fa3c880d9ed
WahajHassan883 <wahajhassan007@gmail.com>
2026-03-03 10:03:52 +0500

    feat: add google analytics (gtag.js) to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Tinos } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/common/navbar";
 import { Footer } from "@/components/common/footer";
@@ -102,6 +103,19 @@ export default function RootLayout({
       <body
         className={`${tinos.variable} font-sans antialiased`}
         suppressHydrationWarning>
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NYY4K41WWM"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NYY4K41WWM');
+          `}
+        </Script>
         <Navbar />
         {children}
         <ScrollToTop />
